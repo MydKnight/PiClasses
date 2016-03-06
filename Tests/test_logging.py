12575e57e810a2ec16ab82e824c9bc78b5894136
madsens <shiloh.madsen@gmail.com>
2016-03-05 20:15:54 -0500

    Testing Heartbeat code.

--- Tests/test_logging.py
+++ Tests/test_logging.py
@@ -5,16 +5,16 @@ import time
 
 dbConn = Logging.Logging()
 
-dbConn.logBoot()
+#dbConn.logBoot()
 
-#numTimesToRepeat = 5
-#while True:
-#    dbConn.logHeartbeat()
-#    numTimesToRepeat -= 1
-#    if numTimesToRepeat==0:
-#        break
-#    else:
-#        time.sleep(5)
+numTimesToRepeat = 5
+while True:
+    dbConn.logHeartbeat()
+    numTimesToRepeat -= 1
+    if numTimesToRepeat==0:
+        break
+    else:
+        time.sleep(5)
 
 #dbConn.logAccess(55555)
 
