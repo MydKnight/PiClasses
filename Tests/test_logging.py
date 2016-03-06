38838a7f12c96ae76758ebaf624c386ec885f89a
madsens <shiloh.madsen@gmail.com>
2016-03-05 20:38:56 -0500

    Testing Access code

--- Tests/test_logging.py
+++ Tests/test_logging.py
@@ -7,15 +7,15 @@ dbConn = Logging.Logging()
 
 #dbConn.logBoot()
 
-numTimesToRepeat = 5
-while True:
-    dbConn.logHeartbeat()
-    numTimesToRepeat -= 1
-    if numTimesToRepeat==0:
-        break
-    else:
-        time.sleep(5)
+#numTimesToRepeat = 5
+#while True:
+#    dbConn.logHeartbeat()
+#    numTimesToRepeat -= 1
+#    if numTimesToRepeat==0:
+#        break
+#    else:
+#        time.sleep(5)
 
-#dbConn.logAccess(55555)
+dbConn.logAccess(55555)
 
 print "done"
