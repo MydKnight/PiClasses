ad593a2736ce0156694140d8d233f7f84c9ef048
Shiloh <shiloh.madsen@gmail.com>
2016-10-19 21:37:59 -0700

    New Gags added

--- HallOfFame.py
+++ HallOfFame.py
@@ -7,16 +7,47 @@ sys.path.append("/home/pi/PiClasses")
 import Logging
 from termios import tcflush, TCIOFLUSH
 from DmxPy import DmxPy
+from random import randint
 
 #Instantiate Logging and DMX Classes
 dbConn = Logging.Logging()
 dmx = DmxPy('/dev/ttyUSB0')
 
-dmx.setChannel(6, 255)
-dmx.setChannel(7, 255)
-dmx.setChannel(8, 159)
-dmx.setChannel(10, 40)
-dmx.render()
+def standBy():
+    offset = 0
+    for x in range(1, 10):
+        rndColor = randint(4, 255)
+        dmx.setChannel(4 + offset, 204)
+        dmx.setChannel(5 + offset, 64)
+        dmx.setChannel(7 + offset, 127)
+        dmx.setChannel(8 + offset, 0)
+        dmx.setChannel(9 + offset, 0)
+        dmx.setChannel(10 + offset, 0)
+
+        offset += 20
+
+    dmx.render()
+    return
+
+def disco():
+    time.sleep(5)
+
+    for x in range (0, 40):
+        offset = 0
+        for x in range (1,10):
+            rndColor = randint(4,255)
+
+            dmx.setChannel(8+offset, 222)
+            dmx.setChannel(7+offset, 255)
+            dmx.setChannel(9 + offset, rndColor)
+
+            offset += 20
+
+        dmx.render()
+        time.sleep(.5)
+    return
+
+standBy()
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
     n = raw_input("Scanned ID: ")
@@ -26,8 +57,11 @@ while True:    # Runs until break is encountered. We want to set it to break on 
     else :
         dbConn.logAccess(n)
 
-
+        disco()
+        standBy()
 
         #flush keyboard buffer
         sys.stdout.flush();
         tcflush(sys.stdin, TCIOFLUSH)
+
+
